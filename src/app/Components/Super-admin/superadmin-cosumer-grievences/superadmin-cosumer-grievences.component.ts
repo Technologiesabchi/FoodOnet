7abe03ccb39e9a054a03e786bbe5e8cd6800a58c
Sayan Mitra <sayan.mitra162@gmail.com>
2023-03-17 17:13:26 +0530

    chanages

---
+++ src/app/Components/Super-admin/superadmin-cosumer-grievences/superadmin-cosumer-grievences.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-superadmin-cosumer-grievences',
+  templateUrl: './superadmin-cosumer-grievences.component.html',
+  styleUrls: ['./superadmin-cosumer-grievences.component.css']
+})
+export class SuperadminCosumerGrievencesComponent {
+
+}
